c9d0162e534850273db67adfa156a11ef800bcf8
Raghavendra <raghavyadav990@gmail.com>
2016-11-10 18:30:07 +0530

    problem 4 pincode update

--- problem-4/p4.py
+++ problem-4/p4.py
@@ -11,14 +11,15 @@ def scrapper():
 
     for i in range(10):
         items  = {}
+        items['product_id'] = tree.xpath('//div[@class="products-grid"]/div[%s]/a/input/@id'%(i+1))
         items["url"] = tree.xpath('//div[@class="products-grid"]/div[%s]/a/@href'%(i+1))
         items["thumbnails"] = tree.xpath('//div[@class="products-grid"]/div[%s]/a/input/@value'%(i+1))
         items["title"] = tree.xpath('//div[@class="products-grid"]/div[%s]//div[@class="details"]/a/text()'%(i+1))
         items["price"] = tree.xpath('//div[@class="products-grid"]/div[%s]//span[@class="price"]/text()'%(i+1))
+        
 
+        items["availabilty"] = [{"picode":i , "availabilty":json.loads(requests.get("http://www.shopclues.com/nss.php?pincode_no=%s&product_ids=%s"%(i , items['product_id'][0])).text)}for i in [671551 , 575001 , 560070] ]
         data["items"].append(items)
-
     return json.dumps(data)
 
 print scrapper()
-
