b0a112ea2182d79bcb606ee4f2128ef4e19351b0
Raghavendra <raghavyadav990@gmail.com>
2016-11-10 17:05:04 +0530

    problem 2 changes

---
+++ problem-2/dataStorer.py
@@ -0,0 +1,30 @@
+import requests
+
+class manage:
+    def __init__(self , host):
+        self.host = host
+
+    def ADD(self , key  , value):
+        headers = {
+            'Content-Type': 'application/json',
+        }
+        data = '{"key":"%s", "value":"%s"}'%(key , value)
+        r = requests.post('%s/add_key/'%(self.host), headers=headers, data=data)
+        return r.text
+
+    def GET(self , key  ):
+        headers = {
+            'Content-Type': 'application/json',
+        }
+        data = '{"key":"%s"}'%(key )
+        r = requests.post('%s/get_key/'%(self.host), headers=headers, data=data)
+        return r.text
+
+    def DELETE(self , key):
+        headers = {
+            'Content-Type': 'application/json',
+        }
+        data = '{"key":"%s"}'%(key )
+        r = requests.post('%s/delete_key/'%(self.host), headers=headers, data=data)
+        return r.text
+
